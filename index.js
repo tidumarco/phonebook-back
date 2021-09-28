2bdf1f92709ea228d127cc40b84e71d729eee263
Marco Tidu <matidu@utu.fi>
2021-09-28 19:28:05 +0300

    Fixed delete again

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.get('/api/persons/:id', (request, response) => {
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(person => person.id !== id)
-
+    response.json(person)
     response.status(204).end()
 })
 
