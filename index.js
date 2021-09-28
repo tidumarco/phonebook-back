6c9ec2db13443baf5af494521aa7f776abe9670d
Marco Tidu <matidu@utu.fi>
2021-09-28 14:40:55 +0300

    second commit

--- index.js
+++ index.js
@@ -7,26 +7,26 @@ app.use(cors())
 app.use(express.json())
 
 let persons =  [
-          {
-            name: "Arto Hellas",
-            number: "040-123456",
-            id: 1
-          },
-          {
-            name: "Martti Tienari",
-            number: "040-123456",
-            id: 2
-          },
-          {
-            name: "Arto Järvinen",
-            number: "040-123456",
-            id: 3
-          },
-          {
-            name: "Lea Kutvonen",
-            number: "040-123456",
-            id: 4
-          }
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
+  },
+  {
+    name: "Martti Tienari",
+    number: "040-123456",
+    id: 2
+  },
+  {
+    name: "Arto Järvinen",
+    number: "040-123456",
+    id: 3
+  },
+  {
+    name: "Lea Kutvonen",
+    number: "040-123456",
+    id: 4
+  }
 ]
 
 app.post('/api/persons', (request, response) => {
